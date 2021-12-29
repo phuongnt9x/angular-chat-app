6771c9471c87940fd56aafa9f37801a42021b21d
phuongnt9x <95663567+phuongnt9x@users.noreply.github.com>
2021-12-29 17:17:32 +0700

    update

--- src/app/service/chat.service.ts
+++ src/app/service/chat.service.ts
@@ -7,72 +7,127 @@ import { Message } from '../message';
 export class ChatService {
   chat: Message[]=[{
     id:1561165,
-    content:"Hello string",
+    content:"Thời gian đằng đẵng",
     sender_id: 1,
     group_id: 3,
     last_message: "Hello"
 
   },{
     id:1561235,
-    content:"Hello abca",
+    content:"Không gian mênh mông",
     sender_id: 2,
     group_id: 3,
     last_message: "Hello"
 
   },{
     id:1561238,
-    content:"Hello abca",
+    content:"Đất Nước là nơi dân mình đoàn tụ",
     sender_id: 2,
     group_id: 3,
     last_message: "Hello"
 
   },{
     id:1561235,
-    content:"Hello ",
+    content:"Đất là nơi Chim về",
     sender_id: 2,
     group_id: 3,
     last_message: "1"
 
   },{
     id:1561215,
-    content:"Hello 111",
+    content:"Nước là nơi Rồng ở",
     sender_id: 3,
     group_id: 3,
     last_message: "2"
 
   },{
     id:1561235,
-    content:"Hello wqe",
+    content:"Những ai bây giờ",
     sender_id: 1,
     group_id: 3,
     last_message: "Hello"
   },{
     id:1562235,
-    content:"Hello ggg",
+    content:"Yêu nhau và sinh con đẻ cái",
     sender_id: 3,
     group_id: 3,
     last_message: "Hello"
 
   },{
     id:1561211,
-    content:"Hello qw",
+    content:"Gánh vác phần người đi trước để lại",
     sender_id: 3,
     group_id: 3,
     last_message: "2"
 
   },{
     id:1561212,
-    content:"Hello qw",
+    content:"Dặn dò con cháu chuyện mai sau",
     sender_id: 3,
     group_id: 2,
     last_message: "2"
 
   },{
     id:1561213,
-    content:"Hello zx",
+    content:"Hằng năm ăn đâu làm đâu",
     sender_id: 3,
     group_id: 2,
     last_message: "2"
+
+  },{
+    id:1561299,
+    content:"Cũng biết cúi đầu nhớ ngày giỗ Tổ",
+    sender_id: 3,
+    group_id: 2,
+    last_message: "2"
+
+  },{
+    id:1562213,
+    content:"Đất là nơi anh đến trường",
+    sender_id: 1,
+    group_id: 1,
+    last_message: "Hello"
+
+  },{
+    id:1562214,
+    content:"Nước là nơi em tắm",
+    sender_id: 2,
+    group_id: 1,
+    last_message: "Hello"
+
+  },{
+    id:1562215,
+    content:"Đất Nước là nơi ta hò hẹn",
+    sender_id: 2,
+    group_id: 1,
+    last_message: "Hello"
+
+  },{
+    id:1562216,
+    content:"Đất Nước là nơi em đánh rơi chiếc khăn trong nỗi nhớ thầm",
+    sender_id: 2,
+    group_id: 1,
+    last_message: "1"
+
+  },{
+    id:1562217,
+    content:"Đất là nơi con chim phượng hoàng bay về hòn núi bạc",
+    sender_id: 3,
+    group_id: 1,
+    last_message: "2"
+
+  },{
+    id:1562218,
+    content:"Khi hai đứa cầm tay",
+    sender_id: 1,
+    group_id: 1,
+    last_message: "Hello"
+  },{
+    id:1562219,
+    content:"Con sẽ mang đất nước đi xa",
+    sender_id: 3,
+    group_id: 2,
+    last_message: "Hello"
 
   }]
   constructor() { }
